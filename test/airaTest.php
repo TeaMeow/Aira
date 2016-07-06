9464e35e56b97c56986d73901b50a771c5666f7a
Yami Odymel <yamiodymel@gmail.com>
2016-07-06 14:22:10 +0000

    More unit tests.

--- test/airaTest.php
+++ test/airaTest.php
@@ -5,12 +5,61 @@ class AiraTest extends \PHPUnit_Framework_TestCase
 {
     function testAdd()
     {
-        Aira::addSuccess('INVALID_TEST', '測試。', 404);
+        Aira::add('INVALID_TEST', '測試。', 404);
     }
 
     function testAddSuccess()
     {
         Aira::addSuccess('TEST', '測試。', 200);
+    }
+
+    function testError()
+    {
+        Aira::error('INVALID_TEST');
+    }
+
+    function testSuccess()
+    {
+        Aira::success('TEST');
+    }
+
+    function testEndOrSuccess()
+    {
+        Aira::endOrSuccess('TEST');
+    }
+
+    function testTheStart()
+    {
+        Aira::theStart();
+    }
+
+    function testTheEnd()
+    {
+        Aira::theEnd();
+    }
+
+    function testEndHere()
+    {
+        Aira::endHere();
+    }
+
+    function testAlive()
+    {
+        Aira::alive();
+    }
+
+    function testSetErrorHandler()
+    {
+        Aira::setErrorHandler(function()
+        {
+        });
+    }
+
+    function testSetSuccessHandler()
+    {
+        Aira::setSuccessHandler(function()
+        {
+        });
     }
 }
 ?>
